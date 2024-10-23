19ba890ac4bdc176ee28a781081ef507ea11aa47
Drofseh <Drofseh@users.noreply.github.com>
2024-10-22 17:57:02 -0700

    use QUOTE in WaveInit

--- addons/ai/modules/waveSpawner.hpp
+++ addons/ai/modules/waveSpawner.hpp
@@ -54,7 +54,11 @@ class GVAR(wavespawn) : Module_F {
             property = QGVAR(wavespawn_WaveInit);
             displayName = "Wave init code";
             tooltip = "Code executed every time a new wave is spawned";
-            expression = "if (_value != '' && {_value != 'params [""_wave"", ""_spawnedGroups"", ""_spawnedUnits"", ""_spawnedVehicles"", ""_spawnedObjects"", ""_logic"", ""_wavehandlerID""];'}) then {[_this, compile _value, true] call FUNC(addWaveHandler);};";
+            expression = QUOTE(                                                                                                                                                                \
+                if (_value != 'params [ARR_7(""_wave"",""_spawnedGroups"",""_spawnedUnits"",""_spawnedVehicles"",""_spawnedObjects"",""_logic"",""_wavehandlerID"")];' && _value != '') then { \
+                    [ARR_3(_this,compile _value,True)] call FUNC(addWaveHandler);                                                                                                              \
+                };                                                                                                                                                                             \
+            );
             defaultValue = "'params [""_wave"", ""_spawnedGroups"", ""_spawnedUnits"", ""_spawnedVehicles"", ""_spawnedObjects"", ""_logic"", ""_wavehandlerID""];'";
             control = "EditCodeMulti5";
         };
